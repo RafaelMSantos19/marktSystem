7d2826e012184b22361e506519a5d8a11c6c9f99
Rafael Henrique Martins dos Santos <rafarag40@gmail.com>
2025-06-25 17:37:37 -0300

    feat: adicionada tela de saida ao precionar Esc

--- electron/main.js
+++ electron/main.js
@@ -2,7 +2,7 @@ require('electron-reload')(__dirname, {
   electron: require(`${__dirname}/node_modules/electron`)
 });
 
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, dialog, ipcMain } = require('electron');
 const path = require('path');
 
 function createWindow () {
@@ -28,4 +28,19 @@ app.whenReady().then(() => {
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') app.quit();
+});
+
+
+ipcMain.on('confirmar-saida', async (event) => {
+  const result = await dialog.showMessageBox({
+    type: 'question',
+    buttons: ['Sim', 'Não'],
+    defaultId: 1,
+    title: 'Confirmação',
+    message: 'Você realmente deseja sair do programa?'
+  });
+
+  if (result.response === 0) {
+    app.quit();
+  }
 });
